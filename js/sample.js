d5090293510950d5983181a0b6906e8648abbe00
DESKTOP-6LJVOB6\ksun0 <ksun0415canada@gmail.com>
2020-12-11 14:33:05 +0900

    スライダーの高さ、ドロワーの高さ、文字画像の調整、コードの位置の調整を変更完了

--- js/sample.js
+++ js/sample.js
@@ -11,7 +11,7 @@ $(function(){
 
 	console.log(h);
 
-	$('#drower-bg').css('top',h);
+	$('#drower-bg').css('top',h -1);
 	
 
 
@@ -39,3 +39,24 @@ $(function() {
 		$('#drower-bg').fadeOut(600);
 	  }
 	}
+
+// slider
+$(function(){
+	var $setElm = $('.viewer'),
+	fadeSpeed = 1500,
+	switchDelay = 3000;
+
+	$setElm.each(function(){
+		var targetObj = $(this);
+		var findUl = targetObj.find('ul');
+		var findLi = targetObj.find('li');
+		var findLiFirst = targetObj.find('li:first');
+
+		findLi.css({display:'block',opacity:'0',zIndex:'99'});
+		findLiFirst.css({zIndex:'100'}).stop().animate({opacity:'1'},fadeSpeed);
+
+		setInterval(function(){
+			findUl.find('li:first-child').animate({opacity:'0'},fadeSpeed).next('li').css({zIndex:'100'}).animate({opacity:'1'},fadeSpeed).end().appendTo(findUl).css({zIndex:'99'});
+		},switchDelay);
+	});
+});
